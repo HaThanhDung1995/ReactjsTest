3d2f40bf9f99701b7acd575faa2325c85014c241
dung.ht@tinhhoa.asia <dung.ht@tinhhoa.asia>
2022-10-11 20:46:47 +0700

    first commit

---
+++ project_1_reactjs/src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+//import { BrowserRouter as Router, Route } from 'react-router-dom';
+//import {Routes, Route} from 'react-router-dom';
+// const menus = [
+//   {
+//     name : 'Home',
+//     to : '/',
+//     exact : true
+//   },
+//   {
+//     name : 'Introduction',
+//     to : '/about',
+//     exact : false
+//   },
+//   {
+//     name : 'Contact',
+//     to : '/contact',
+//     exact : false
+//   }
+// ];
+// const MenuLink = ({label,to,activeOnlyWhenExact}) => {
+//   return (
+//     <Route path={to} exact={activeOnlyWhenExact} children={(match) => {
+//       var aticve = match ? 'active' : '';
+//       return (
+        
+//       )
+//     }}/>
+//   )
+// }
+export default class App extends Component {
+  // showMenus = (menus) => {
+  //   var result = null;
+  //   if(menus.length > 0) {
+  //     result = menus.map((menu,index) => {
+
+  //     });
+  //   }
+  //   return result;
+  // }
+  render() {
+    return (
+      // <Router></Router>
+      <div>
+          {/* <Route path="/" element={}></Route> */}
+      </div>
+    )
+  }
+}
